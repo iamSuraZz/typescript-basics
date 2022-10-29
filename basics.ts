6ba53f403b6213bc143f05d2784f3460bcccb5cf
SuraZz007 <surajdev9886@gmail.com>
2022-10-29 18:00:38 +0530

    initialize primitive values

---
+++ basics.ts
@@ -0,0 +1,16 @@
+// Primitives : number, string, boolean
+// More comples types : arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 12.2;
+
+let userName: string;
+userName = "Suraj";
+
+let isInstructor: boolean;
+
+isInstructor = true;
