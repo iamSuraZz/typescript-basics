08c52a06d51018afbb0728abbd19486c38c1cb87
SuraZz007 <surajdev9886@gmail.com>
2022-10-30 12:29:43 +0530

    Type inference in typescript

--- basics.ts
+++ basics.ts
@@ -14,3 +14,39 @@ userName = "Suraj";
 let isInstructor: boolean;
 
 isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+let val: number[];
+
+hobbies = ["Sports", "Cooking"];
+val = [1];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Ram",
+  age: 20,
+};
+
+// not correct
+
+// person = {
+// isEmployee : true   // person accepts only string and number
+// }
+
+// below accpts array of values having string and number
+let people: {
+  name: string;
+  age: number;
+}[];
+
+// Type inference
+
+let course = "React - The Complete Guide";
+
+// course = 124;
